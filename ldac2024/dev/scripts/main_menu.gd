99a1e496ff0fea5a133b002cca1d03e6c1556014
pudungo <yunisnabiyev@gmail.com>
2024-11-24 15:59:39 -0800

    fixes

--- ldac2024/dev/scripts/main_menu.gd
+++ ldac2024/dev/scripts/main_menu.gd
@@ -1,7 +1,8 @@
 extends Control
 
+@onready var start = $button_container/test_scene_button
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	$button_container/test_scene_button.pressed.connect(func(): WRAPPER.change_to_scene(WRAPPER.SCENES.DEV))
-	pass # Replace with function body.
+	start.pressed.connect(func(): WRAPPER.change_to_scene(WRAPPER.SCENES.DEV))
